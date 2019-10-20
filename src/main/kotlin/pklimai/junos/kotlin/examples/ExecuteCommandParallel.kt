45c04de03a4c518a14ee33b587aeaeba1f436d4e
Peter Klimai <pklimai@gmail.com>
2019-10-21 01:51:33 +0300

    Parallel execution example added

--- src/main/kotlin/pklimai/junos/kotlin/examples/ExecuteCommandParallel.kt
+++ src/main/kotlin/pklimai/junos/kotlin/examples/ExecuteCommandParallel.kt
@@ -2,30 +2,69 @@ package pklimai.junos.kotlin.examples
 
 import kotlinx.coroutines.*
 import pklimai.junos.kotlin.lib.buildDevice
+import pklimai.junos.kotlin.lib.execute
 
 /*
-  Similar to ExecuteCommand.kt, but using coroutines
-  TODO: not working as it should yet
- */
+  Similar to ExecuteCommand.kt, but using Kotlin coroutines
+  Note: Parallel execution works thanks to coroutines using different threads, as NETCONF library
+  is using blocking I/O
+
+  Example output:
+
+[DefaultDispatcher-worker-4] INFO net.juniper.netconf.Device - Connecting to host 10.254.0.41 on port 830.
+[DefaultDispatcher-worker-1] INFO net.juniper.netconf.Device - Connecting to host 10.254.0.37 on port 830.
+[DefaultDispatcher-worker-2] INFO net.juniper.netconf.Device - Connecting to host 10.254.0.42 on port 830.
+[DefaultDispatcher-worker-3] INFO net.juniper.netconf.Device - Connecting to host 10.254.0.35 on port 830.
+[DefaultDispatcher-worker-2] INFO net.juniper.netconf.Device - Connected to host 10.254.0.42 - Timeout set to 5000 msecs.
+[DefaultDispatcher-worker-4] INFO net.juniper.netconf.Device - Connected to host 10.254.0.41 - Timeout set to 5000 msecs.
+[DefaultDispatcher-worker-3] INFO net.juniper.netconf.Device - Connected to host 10.254.0.35 - Timeout set to 5000 msecs.
+[DefaultDispatcher-worker-1] INFO net.juniper.netconf.Device - Connected to host 10.254.0.37 - Timeout set to 5000 msecs.
+10.254.0.35:
+Interface               Admin Link Proto    Local                 Remote
+ge-0/0/0.0              up    up   inet     10.254.0.35/24
+[DefaultDispatcher-worker-3] INFO net.juniper.netconf.Device - Connecting to host 10.254.0.31 on port 830.
+10.254.0.37:
+Interface               Admin Link Proto    Local                 Remote
+ge-0/0/0.0              up    up   inet     10.254.0.37/24
+10.254.0.41:
+Interface               Admin Link Proto    Local                 Remote
+ge-0/0/0.0              up    up   inet     10.1.0.1/24
+                                   multiservice
+10.254.0.42:
+Interface               Admin Link Proto    Local                 Remote
+ge-0/0/0.0              up    up   inet     10.1.0.2/24
+                                   multiservice
+[DefaultDispatcher-worker-3] INFO net.juniper.netconf.Device - Connected to host 10.254.0.31 - Timeout set to 5000 msecs.
+10.254.0.31:
+Interface               Admin Link Proto    Local                 Remote
+ge-0/0/0.0              up    up   inet     10.254.0.31/24
+
+*/
 
 fun main() = runBlocking {
-    val results = mutableMapOf<String, Deferred<String>>()
-
+    val jobs = mutableMapOf<String, Job>()
     for (host in HOSTS_LIST) {
-        results[host] = async {
+        jobs[host] = GlobalScope.launch {
+
+            buildDevice(host, USERNAME, PASSWORD).execute {
+                println(host + ":\n" + runCliCommand("show interfaces ge-0/0/0.0 terse"))
+            }
+
+            // Does the same as above, but you get explicit warning 'Inappropriate blocking
+            // method call' from the IDE
+            /*
             val dev = buildDevice(host, USERNAME, PASSWORD)
             dev.connect()
             val res = dev.runCliCommand("show interfaces ge-0/0/0.0 terse")
             dev.close()
-            println("Closing $host")
-            res
+            println("$host:\n$res")
+            */
+
         }
     }
 
     for (host in HOSTS_LIST) {
-        val output = results[host]!!.await()
-        println(host)
-        println(output)
-        println()
+        jobs[host]!!.join()
     }
+
 }
--- src/main/kotlin/pklimai/junos/kotlin/examples/labEnvironment.kt
+++ src/main/kotlin/pklimai/junos/kotlin/examples/labEnvironment.kt
@@ -6,4 +6,4 @@ const val USERNAME = "lab"
 const val PASSWORD = "lab123"
 
 // List of lab devices for parallel execution test
-val HOSTS_LIST = listOf("10.254.0.41", "10.254.0.42", "10.254.0.35", "10.254.0.37")
+val HOSTS_LIST = listOf("10.254.0.41", "10.254.0.42", "10.254.0.35", "10.254.0.37", "10.254.0.31")
